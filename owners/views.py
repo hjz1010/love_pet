c55a36ce36643f7dc695c264b4e6f75f09ee7e71
melony <hjz1010@gmail.com>
2022-07-05 10:21:34 +0900

    ADD : POST models.py & views.py 완성

--- owners/views.py
+++ owners/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
 
+import json
+from django.http import JsonResponse
+
+from django.shortcuts import render
+from django.views import View
+
+from owners.models import Owner, Dog
 # Create your views here.
+
+
+class OwnersView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        Owner.objects.create(
+            name=data['name'],
+            email=data['email'],
+            age=data['age'])
+        return JsonResponse({'message': 'created'}, status=201)
+
+
+class DogsView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        Dog.objects.create(
+            name=data['name'],
+            age=data['age'],
+            owner=Owner.objects.get(name=data['owner'])
+        )
+        return JsonResponse({'message': 'created'}, status=201)
